b1223472b0c8c079be963838d456f2d9f81bf946
xdream oldlu <xdream@gmail.com>
2024-12-26 16:32:54 +0800

    multi logo icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,11 @@ export const metadata: Metadata = {
 	description: "born to serve pyBlobServer",
 	icons: {
 		apple: "/apple-touch-icon.png",
-	}
+		icon: [
+			{ rel: "icon", url: "/logo-icon.svg", type: "image/svg" },
+			{ rel: "icon", url: "/logo-icon.png", type: "image/png" },
+		],
+	},
 }
 
 export default function RootLayout({
